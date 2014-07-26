5375cf1efb449873ed34398a4953daba6760a1a9
unknown <swarhade@A-08B-SWARDAD-0.dts.local>
2014-07-26 10:18:27 -0700

    Removed unwanted HTML output condition.

--- qa-custom-404-page-layer.php
+++ qa-custom-404-page-layer.php
@@ -5,35 +5,17 @@ class qa_html_theme_layer extends qa_html_theme_base
 
         function main()
         {
-            if ($this->template==='not-found') 
+            if (($this->template==='not-found') && (qa_opt('pt_enable_html_404_message'))) 
 			{
-                // output the content of your 404 page...
-				if (qa_opt('pt_enable_html_404_message'))
-				{
-								$this->output('<div class="qa-main">');
-								$this->output(qa_opt('pt_q2a_html_404_message_codebox'));
-								$this->output('</div>');
-				}
-				else
-				{
-					$this->output('<div class="qa-main">
-						<h1>
-						</h1>
-						<div class="qa-error">
-							Page not found
-						</div>
-						<div class="qa-suggest-next">
-							To see more, click for the <a href="./questions">full list of questions</a> or <a href="./tags">popular tags</a>.
-						</div>
-					</div>');
-				}
+				$this->output('<div class="qa-main">');
+				$this->output(qa_opt('pt_q2a_html_404_message_codebox'));
+				$this->output('</div>');
             } 
 			else
 			{
                 qa_html_theme_base::main();
 			}
         }
-
 }
 /*
 	Omit PHP closing tag to help avoid accidental output
